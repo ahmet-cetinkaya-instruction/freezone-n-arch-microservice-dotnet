f369366f637038b4400ee93e9b3040f2d461814e
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-05-21 12:03:01 +0300

    initial commit

---
+++ src/orderService/Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence.Contexts;
+
+namespace Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<BaseDbContext>(options => options.UseInMemoryDatabase("nArchitecture"));
+
+        return services;
+    }
+}
