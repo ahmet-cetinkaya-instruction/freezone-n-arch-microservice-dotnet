c3d9e7b95effc8b9c411b148c752be91dafda6f0
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-05-22 10:23:02 +0300

    feat(Core.EventBus): add integration event and integration event handler

---
+++ src/corePackages/Core.EventBus/Abstraction/IIntegrationEventHandler.cs
@@ -0,0 +1,11 @@
+﻿using Core.EventBus.Events;
+
+namespace Core.EventBus.Abstraction;
+
+public interface IIntegrationEventHandler { } // Marker interface
+
+public interface IIntegrationEventHandler<TIntegrationEvent> : IIntegrationEventHandler
+    where TIntegrationEvent : IntegrationEvent
+{
+    Task Handle(TIntegrationEvent @event);
+}
---
+++ src/corePackages/Core.EventBus/Events/IntegrationEvent.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace Core.EventBus.Events;
+
+public class IntegrationEvent
+{
+    [JsonProperty]
+    public Guid Id { get; }
+
+    [JsonProperty]
+    public DateTime CreatedDate { get; }
+
+    public IntegrationEvent()
+    {
+        Id = Guid.NewGuid();
+        CreatedDate = DateTime.UtcNow;
+    }
+
+    [JsonConstructor]
+    public IntegrationEvent(Guid id, DateTime createdDate)
+    {
+        Id = id;
+        CreatedDate = createdDate;
+    }
+}
