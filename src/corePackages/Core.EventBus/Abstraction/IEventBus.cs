67ced534d01b75bde94aa8930f925a054bb0d1c3
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-05-22 10:24:02 +0300

    feat(Core.EventBus): add event bus interface

---
+++ src/corePackages/Core.EventBus/Abstraction/IEventBus.cs
@@ -0,0 +1,19 @@
+﻿using Core.EventBus.Events;
+
+namespace Core.EventBus.Abstraction;
+
+public interface IEventBus
+{
+    void Publish(IntegrationEvent @event);
+
+
+    // Belirli bir türedeki IntegrationEvent'leri dinlemek için kullanılır. Olayları ilgili işleyiciye (handler) iletmek için kullanılır.
+    void Subscribe<TIntegrationEvent, IIntegrationEventHandler>()
+        where TIntegrationEvent : IntegrationEvent
+        where IIntegrationEventHandler : IIntegrationEventHandler<TIntegrationEvent>;
+
+    // Belirli bir türedeki IntegrationEvent'leri dinlemekten vazgeçmek için kullanılır.
+    void Unsubscribe<TIntegrationEvent, IIntegrationEventHandler>()
+        where TIntegrationEvent : IntegrationEvent
+        where IIntegrationEventHandler : IIntegrationEventHandler<TIntegrationEvent>;
+} 
