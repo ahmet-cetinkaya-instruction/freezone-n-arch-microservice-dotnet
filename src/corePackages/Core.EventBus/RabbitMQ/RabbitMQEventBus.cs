f486c7fb4105d153b845ac649838cec7def829c4
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-05-24 10:50:22 +0300

    feat(Core.EventBus): start adding rabbitmq persistent connection

---
+++ src/corePackages/Core.EventBus/RabbitMQ/RabbitMQEventBus.cs
@@ -0,0 +1,17 @@
+﻿using Core.EventBus.Events;
+
+namespace Core.EventBus.RabbitMQ;
+
+public class RabbitMQEventBus : BaseEventBus
+{
+    private readonly RabbitMQPersistentConnection _connection;
+
+    public RabbitMQEventBus(EventBusConfig eventBusConfig, IServiceProvider serviceProvider)
+        : base(eventBusConfig, serviceProvider) { }
+
+    public override void Publish(IntegrationEvent @event) => throw new NotImplementedException();
+
+    public override void Subscribe<TIntegrationEvent, TIntegrationEventHandler>() => throw new NotImplementedException();
+
+    public override void Unsubscribe<TIntegrationEvent, TIntegrationEventHandler>() => throw new NotImplementedException();
+}
---
+++ src/corePackages/Core.EventBus/RabbitMQ/RabbitMQPersistentConnection.cs
@@ -0,0 +1,44 @@
+﻿using Polly;
+using Polly.Retry;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using System.Net.Sockets;
+
+namespace Core.EventBus.RabbitMQ;
+
+public class RabbitMQPersistentConnection
+{
+    public bool IsConnected => _connection != null && _connection.IsOpen;
+
+    // Her bir connection'ın açık olup olmadığını kontrol eder.
+    private IConnection? _connection; // RabbitMQ.Client
+    private readonly IConnectionFactory _connectionFactory; // RabbitMQ.Client
+    private readonly int _retryCount;
+
+    public RabbitMQPersistentConnection(IConnectionFactory connectionFactory, int retryCount)
+    {
+        _connectionFactory = connectionFactory;
+        _retryCount = retryCount;
+    }
+
+    public bool TryConnect()
+    {
+        // Tekrar bağlanma süreçlerinde nasıl davranması gerektiğini belirtir.
+        RetryPolicy connectionPolicy = Policy // Polly.Retry
+            .Handle<SocketException>()
+            .Or<BrokerUnreachableException>()
+            .WaitAndRetry(
+                _retryCount,
+                sleepDurationProvider: retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+                onRetry: (ex, time) => { }
+            );
+
+        // Yeniden deneme politikasıyla bağlantı deneme gerçekleşir.
+        connectionPolicy.Execute(() =>
+        {
+            _connection = _connectionFactory.CreateConnection();
+        });
+
+        return true;
+    }
+}
