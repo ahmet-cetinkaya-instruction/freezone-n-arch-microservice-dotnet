7a6bd0ae7ab12685444d61776d2b576e65ffd9cb
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-05-25 11:49:54 +0300

    feat(Core.EventBus): add event bus factory

---
+++ src/corePackages/Core.EventBus/Factory/EventBusFactory.cs
@@ -0,0 +1,27 @@
+﻿using Core.EventBus.Abstraction;
+using Core.EventBus.Events;
+using Core.EventBus.RabbitMQ;
+
+namespace Core.EventBus.Factory;
+
+public static class EventBusFactory
+{
+    public static IEventBus Create(EventBusConfig config, IServiceProvider serviceProvider) =>
+        config.EventBusType switch
+        {
+            // EventBusType.AzureServiceBus => new AzureServiceBusEventBus(config, serviceProvider),
+            EventBusType.RabbitMQ
+                => new RabbitMQEventBus(config, serviceProvider),
+            _ => throw new ArgumentOutOfRangeException()
+        };
+
+    //switch(config.EventBusType)
+    //{
+    //    // case EventBusType.AzureServiceBus:
+    //    //     return new AzureServiceBusEventBus(config, serviceProvider);
+    //    case EventBusType.RabbitMQ:
+    //        return new RabbitMQEventBus(config, serviceProvider);
+    //    default:
+    //        throw new ArgumentOutOfRangeException();
+    //}
+}
