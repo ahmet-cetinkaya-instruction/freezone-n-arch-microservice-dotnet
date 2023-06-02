ed676ad47a21efe69a8d176d7461e0404942745b
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-06-02 11:15:58 +0300

    feat(Core.Consul): add service discovery with consul

---
+++ src/corePackages/Core.Consul/ConsulConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Core.Consul;
+public class ConsulConfiguration
+{
+    public string Address { get; set; }
+
+    public string ServiceAddress { get; set; }
+    public string ServiceId { get; set; }
+    public string ServiceName { get; set; }
+    public string ServiceTags { get; set; }
+}
---
+++ src/corePackages/Core.Consul/ConsulRegistration.cs
@@ -0,0 +1,65 @@
+﻿using Consul;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Core.Consul;
+
+public static class ConsulRegistration
+{
+    // Consul istemcisini ServiceCollection'a ekleriyoruz.
+    public static IServiceCollection AddConsul(this IServiceCollection services, IConfiguration configuration)
+    {
+        const string section = "Consul";
+        ConsulConfiguration consulConfiguration =
+            configuration.GetSection(section).Get<ConsulConfiguration>()
+            ?? throw new ArgumentNullException($"{section} section cannot found in configuration.");
+
+        services.AddSingleton<IConsulClient, ConsulClient>(
+            p =>
+                new ConsulClient(consulConfig =>
+                {
+                    consulConfig.Address = new Uri(consulConfiguration.Address);
+                })
+        );
+
+        return services;
+    }
+
+    // İlgili servisi Consul'a kayıt etme işlemini gerçkleştiriyoruz. Ve aynı zamanda da proje durdururken de bu durumu consul'a bildiriyoruz.
+    public static IApplicationBuilder UseConsul(
+        this IApplicationBuilder app,
+        IHostApplicationLifetime lifeTime,
+        ConsulConfiguration configuration
+    )
+    {
+        // Gerekli servisleri aliyoruz.
+        IConsulClient consulClient = app.ApplicationServices.GetRequiredService<IConsulClient>();
+
+        // Çalışma adresini almamız gerekiyor.
+        Uri serviceAddress =
+            new(app.ServerFeatures.Get<IServerAddressesFeature>().Addresses.FirstOrDefault() ?? configuration.ServiceAddress);
+        // Consul'a servisi kayıt ediyoruz.
+        AgentServiceRegistration registration =
+            new()
+            {
+                ID = configuration.ServiceId,
+                Name = configuration.ServiceName,
+                Address = serviceAddress.Host,
+                Port = serviceAddress.Port,
+                Tags = new[] { configuration.ServiceId, configuration.ServiceName }.Union(configuration.ServiceTags.Split(",")).ToArray(),
+            };
+        consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+        consulClient.Agent.ServiceRegister(registration).Wait();
+
+        // Uygulama durdurulduğunda Consul'dan servisi siliyoruz.
+        lifeTime.ApplicationStopping.Register(() =>
+        {
+            consulClient.Agent.ServiceDeregister(registration.ID).Wait();
+        });
+
+        return app;
+    }
+}
