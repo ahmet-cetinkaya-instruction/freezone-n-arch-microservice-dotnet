d82ac11c95d3b162f3070c08196088d26e26798d
Ahmet Çetinkaya <ahmetcetinkaya7@outlook.com>
2023-06-02 09:53:11 +0300

    feat(graylog): add gray log

---
+++ src/corePackages/Core.CrossCuttingConcerns/Logging/Serilog/Logger/GraylogLogger.cs
@@ -0,0 +1,30 @@
+﻿using Core.CrossCuttingConcerns.Logging.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Logging.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Sinks.Graylog;
+using Serilog.Sinks.Graylog.Core.Transport;
+
+namespace Core.CrossCuttingConcerns.Logging.Serilog.Logger;
+
+public class GraylogLogger : LoggerServiceBase
+{
+    public GraylogLogger(IConfiguration configuration)
+    {
+        const string configurationSection = "SeriLogConfigurations:GraylogConfiguration";
+        GraylogConfiguration config =
+            configuration.GetSection(configurationSection).Get<GraylogConfiguration>()
+            ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        Logger = new LoggerConfiguration().WriteTo
+            .Graylog(
+                new GraylogSinkOptions()
+                {
+                    HostnameOrAddress = config.HostnameOrAddress,
+                    Port = config.Port,
+                    TransportType = TransportType.Udp
+                }
+            )
+            .CreateLogger();
+    }
+}
